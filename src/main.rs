2c91b97668f076aef16ff3a75b858ee45ad29a9f
joeloverbeck <joeloverbeck@gmail.com>
2020-08-04 09:03:53 +0200

    Added first test.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,41 @@
 
 
-fn main() {
-    println!("Hello, world!");
+trait Track {
 
+}
+
+struct NvaStats {
+    victory_level: u8
+}
+
+impl NvaStats{
+    fn new () -> NvaStats {
+        NvaStats {
+            victory_level: 0
+        }
+    }
+
+    fn get_victory_level(&self) -> u8 {
+        self.victory_level
+    }
+}
+
+pub fn main() {
+ println!("This sorta works.");
+
+ 
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_on_creation_nva_victory_level_should_be_zero() {
+
+        let nva_stats = NvaStats::new();
+
+        assert_eq!(nva_stats.get_victory_level(), 0);
+    }
 }
